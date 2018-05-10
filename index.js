d48fc972d8213dc2f0411908b6e242f4367eebb8
m4rquee <lucasxm21@hotmail.com>
2018-05-10 00:30:09 -0300

    Searching plates

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 const _ = require('lodash');
 const rp = require('request-promise');
 const { generate } = require("randomstring");
+const { gen_plates } = require('./gen_plates');
 
-const num = 100;
-const esp = 50;
-const tamBatch = 1000;
-
-function ger_placa() {
-    let placaLet = generate({ length: 3, charset: 'alphabetic' }).toLowerCase();
-    let placaNum = generate({ length: 4, charset: 'numeric' });
-    return 'qfs9083';
-    return placaLet + '' + placaNum;
-}
+const waitTime = 50;
+const batchSize = 100;
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -31,40 +24,50 @@ var options = {
     }
 };
 
-async function getCarros() {
-    let carros = [[], []];
+async function getNCars(n, plates) {
+    let cars = [[], []];
 
-    while (carros[0].length < num) {
-        let novaPlaca;
+    while (true) { //(cars[0].length < n) {
         let batch = [[], []];
-        while (batch[0].length < Math.min(num - carros[0].length, tamBatch)) {
-            novaPlaca = ger_placa();
-            if (!carros[0].includes(novaPlaca) && !batch[0].includes(novaPlaca)) {
-                options.form.placa = novaPlaca;
-                batch[0].push(novaPlaca);
-                batch[1].push(rp(options));
-                //await sleep(esp);
-            }
-        }
+        while (batch[0].length < Math.min(n - cars[0].length, batchSize)) {
+            let plate = plates.next();
+            if (!plate)
+                return cars;
 
-        console.log('Fazendo requests');
+            for (let i = 0; i < 100; i++)
+                plates.next();
+            
+
+            options.form.placa = plate[0] + plate[1];
+
+            batch[0].push(plate);
+            batch[1].push(rp(options));
+
+            await sleep(waitTime);
+        }
 
         await Promise.all(batch[1]).then(resp => {
             for (let i = 0; i < resp.length; i++) {
-                if (!resp[i].includes('Licenciamento: <b>    </b>') && !resp[i].includes('<h3>Placa Não Cadastrada')) {
-                    console.log(`${batch[0][i]} achado`);
-                    carros[0].push(batch[0][i]);
-                    carros[1].push(resp[i].split('Licenciamento:')[1]);
+                if (!resp[i].includes('Licenciamento: <b>    </b>')
+                    && !resp[i].includes('<h3>Placa Não Cadastrada')) {
+
+                    console.error(`-----------------------------------------------------${batch[0][i]} found`);
+                    cars[0].push(batch[0][i]);
+                    cars[1].push(resp[i].split('Licenciamento:')[1]);
                 }
             }
-
-            console.log('Batch feito');
         }).catch(err => console.log(err));
+
+        await sleep(waitTime * 10);
+
+        //console.log('Batch done');
+        console.log(`Currently at ${batch[0][0][0]}-${batch[0][0][1]}`)
     }
 
-    return carros;
+    return cars;
 }
 
-getCarros().then((a) => {
-    console.log(...a);
+let plates = gen_plates();
+getNCars(1, plates).then((a) => {
+    a[0].forEach(console.log);
 }).catch(console.error);
